fdd2ada4d13d88cd20986b0dc9334ce2109c7be2
imorti <imorti@gmail.com>
2017-07-06 22:12:07 -0400

    now using redis for sessions.

--- config/session.js
+++ config/session.js
@@ -44,7 +44,7 @@ module.exports.session = {
   *                                                                          *
   ***************************************************************************/
 
-  // adapter: 'redis',
+  adapter: 'redis',
 
   /***************************************************************************
   *                                                                          *
@@ -55,10 +55,10 @@ module.exports.session = {
   *                                                                          *
   ***************************************************************************/
 
-  // host: 'localhost',
-  // port: 6379,
+  host: 'localhost',
+  port: 6379,
   // ttl: <redis session TTL in seconds>,
-  // db: 0,
+  db: 0,
   // pass: <redis auth password>,
   // prefix: 'sess:',
 
--- config/sockets.js
+++ config/sockets.js
@@ -44,11 +44,11 @@ module.exports.sockets = {
   // -OR-
   //
 
-  // adapter: 'socket.io-redis',
-  // host: '127.0.0.1',
-  // port: 6379,
-  // db: 0,
-  // pass: '<redis auth password>',
+  adapter: 'socket.io-redis',
+  host: '127.0.0.1',
+  port: 6379,
+  db: 0,
+  // pass: '',
 
 
 
